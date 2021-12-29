e068e621c6ea38e07cbe4551350c819b8b19a580
Vitaliy Derdel <mogwuy@ya.ru>
2021-12-29 12:31:52 +0300

    Правки после первого ревью

--- routes/movies.js
+++ routes/movies.js
@@ -1,25 +1,42 @@
 const moviesRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const {
   getMovies, createMovie, deleteMovie,
 } = require('../controllers/movies');
 
-moviesRouter.get('/', getMovies);
-moviesRouter.post('/', celebrate({
+moviesRouter.get('/movies', getMovies);
+moviesRouter.post('/movies', celebrate({
   body: Joi.object().keys({
     country: Joi.string().required().min(2).max(60),
     director: Joi.string().required().min(2).max(60),
-    duration: Joi.string().required().min(2).max(4),
-    year: Joi.string().required().min(2).max(4),
+    duration: Joi.number().required(),
+    year: Joi.number().required(),
     description: Joi.string().required().min(2).max(240),
-    image: Joi.string().required().regex(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/),
-    trailer: Joi.string().required().regex(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/),
-    thumbnail: Joi.string().required().regex(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?/),
+    image: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Поле image заполнено некорректно');
+    }),
+    trailer: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Поле trailer заполнено некорректно');
+    }),
+    thumbnail: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Поле thumbnail: заполнено некорректно');
+    }),
     nameRU: Joi.string().required().min(2).max(120),
     nameEN: Joi.string().required().min(2).max(120),
+    moveId: Joi.number().required(),
   }),
 }), createMovie);
-moviesRouter.delete('/:id', celebrate({
+moviesRouter.delete('/movies/:id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().length(24).hex(),
   }),
